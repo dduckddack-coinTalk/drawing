8a9abdf5f67b64704dc1ce2e2dfd6ead3de54ab0
kysyo <surfingboy0914@gamil.com>
2022-05-18 14:02:34 +0900

    feat: Amazon S3 업로드 실패시, 혹은 로컬 파일 생성 실패시 예외처리 추가

--- src/main/java/com/cointalk/drawing/service/DrawingServiceImpl.java
+++ src/main/java/com/cointalk/drawing/service/DrawingServiceImpl.java
@@ -8,6 +8,7 @@ import com.cointalk.drawing.domain.DrawingResponse;
 import com.cointalk.drawing.domain.DrawingResponseInnerData;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.influxdb.InfluxDBException;
 import org.influxdb.dto.BoundParameterQuery;
 import org.influxdb.dto.Point;
 import org.influxdb.dto.Query;
@@ -22,14 +23,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 @RequiredArgsConstructor
@@ -49,51 +48,64 @@ public class DrawingServiceImpl implements DrawingService {
     public DrawingResponse uploadImage(MultiValueMap<String, Part> data) {
         DrawingResponse result = new DrawingResponse();
         Map<String, Object> drawingDataMap = new HashMap<>();
-        try{
+        try {
 
-        data.forEach((arg1, arg2)->{
-            if(!arg1.equals("image")) {	// 파라미터 처리
-                arg2.forEach( body->{
-                    body.content().subscribe(dataBuffer -> {
-                        byte[] bytes = new byte[dataBuffer.readableByteCount()];
-                        dataBuffer.read(bytes);
-                        DataBufferUtils.release(dataBuffer);
-                        String valueFromMultipart = new String(bytes, StandardCharsets.UTF_8);
-                        drawingDataMap.put(arg1, valueFromMultipart);
+            data.forEach((arg1, arg2) -> {
+                if (!arg1.equals("image")) {  // 이미지 파일이 아닌 파라미터 저장
+                    arg2.forEach(body -> {
+                        body.content().subscribe(dataBuffer -> {
+                            byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                            dataBuffer.read(bytes);
+                            DataBufferUtils.release(dataBuffer);
+                            String valueFromMultipart = new String(bytes, StandardCharsets.UTF_8);
+                            drawingDataMap.put(arg1, valueFromMultipart);
+                        });
                     });
-                });
-            } else {  // 이미지 파일 업로드 및 S3 업로드 주소 저장
-                Mono.just(data.get("image"))
-                        .flatMapMany(Flux::fromIterable)
-                        .cast(FilePart.class)
-                        .map(filePart -> {
-                            String filename = String.valueOf(UUID.randomUUID())+filePart.filename();
-                            // 파일파트 받아서 로컬저장소에 저장 -> 로컬 저장소에 저장된 파일로 s3업로드 -> s3파일 업로드 후 로컬 저장소 파일 삭제
-                            filePart.transferTo(Paths.get(localStoragePath + filename)).subscribe();
-                            File files = new File(localStoragePath+filename);
-                            amazonS3Client.putObject(new PutObjectRequest(bucket, filename, files).withCannedAcl(CannedAccessControlList.PublicRead));
-                            if(files.delete()){
-                                log.info("local file delete");
-                            }
-                            String amazonS3Url = amazonS3Client.getUrl(bucket, filename).toString();
-                            drawingDataMap.put(arg1, amazonS3Url);
-                            return filePart;
-                        })
-                        .subscribe();
+                } else {  // formData 로 받은 파일파트 이미지 파일 업로드 및 S3 업로드 주소 저장
+                    Mono.just(data.get("image"))
+                            .flatMapMany(Flux::fromIterable)
+                            .cast(FilePart.class)
+                            .map(filePart -> {
+                                String filename = String.valueOf(UUID.randomUUID()) + filePart.filename();
+                                // 파일파트 받아서 로컬저장소에 저장 (비동기 논블럭킹으로 실행) -> 로컬 저장소에 저장된 파일로 s3업로드 -> s3파일 업로드 후 로컬 저장소 파일 삭제
+                                filePart.transferTo(Paths.get(localStoragePath + filename)).subscribe();
+                                drawingDataMap.put("filename", filename);
+                                return filePart;
+                            })
+                            .subscribe();
+                }
+            });
+
+            String filename = (String) drawingDataMap.get("filename");
+            // 로컬 저장소에 저장된 파일로 s3업로드 -> s3파일 업로드 후 로컬 저장소 파일 삭제
+            File files = checkCreateFile(filename);
+
+            amazonS3Client.putObject(new PutObjectRequest(bucket, filename, files).withCannedAcl(CannedAccessControlList.PublicRead));
+            String amazonS3Url = amazonS3Client.getUrl(bucket, filename).toString();
+
+            for(int i=0; i<3; i++){
+                // s3업로드 완료 확인시 로컬 저장소 파일 삭제. 업로드 실패하면 3번 재시도
+                if(deleteLocalFile(filename, files)){
+                    break;
+                }
             }
-        });
-        // influxDB에 유저, 코인종류, 차트시간, 이미지 주소 저장
-        Point point = Point.measurement("drawing_image")
-                .time(Long.parseLong((String) drawingDataMap.get("time")), TimeUnit.MILLISECONDS)
-                .tag("userId", (String) drawingDataMap.get("userId"))
-                .tag("image", (String) drawingDataMap.get("image"))
-                .addField("coin", (String) drawingDataMap.get("coin"))
-                .build();
-        influxDBTemplate.write(point);
 
-        result.setStatus("ok");
-        result.setMessage(drawingDataMap);
+            // influxDB에 유저, 코인종류, 차트시간, 이미지 주소 저장
+            Point point = Point.measurement("drawing_image")
+                    .time(Long.parseLong((String) drawingDataMap.get("time")), TimeUnit.MILLISECONDS)
+                    .tag("userId", (String) drawingDataMap.get("userId"))
+                    .tag("image", amazonS3Url)
+                    .addField("coin", (String) drawingDataMap.get("coin"))
+                    .build();
+            influxDBTemplate.write(point);
 
+            result.setStatus("ok");
+            result.setMessage(drawingDataMap);
+
+        }catch (InfluxDBException e){
+            result.setStatus("error");
+            result.setMessage("influxDB 에러");
+            e.printStackTrace();
         }catch (Exception e){
             result.setStatus("error");
             result.setMessage("기타에러");
@@ -107,7 +119,9 @@ public class DrawingServiceImpl implements DrawingService {
 
         DrawingResponse result = new DrawingResponse();
         List<DrawingResponseInnerData> resultList = new ArrayList<>();
-        String q = "SELECT * FROM drawing_image where userId= '"+userId+"' order by desc";
+        String q = "SELECT * FROM drawing_image " +
+                "WHERE userId= '"+userId+"' " +
+                "ORDER BY DESC";
 
         Query query = BoundParameterQuery.QueryBuilder.newQuery(q)
                 .forDatabase("coin")
@@ -129,5 +143,44 @@ public class DrawingServiceImpl implements DrawingService {
                     return result;
                 });
     }
+    private File checkCreateFile(String filename) throws InterruptedException {
+        File files = new File(localStoragePath + filename);
+        boolean fileExist = files.exists();
+
+        // 파일생성 체크 후 진행
+        for(int i =0; i<3; i++){
+            if(fileExist){
+                break;
+            }
+            Thread.sleep(300);
+            fileExist = files.exists();
+        }
+
+        return files;
+    }
+
+    private boolean deleteLocalFile(String filename, File files) throws InterruptedException {
+
+        if (amazonS3Client.doesObjectExist(bucket, filename)) { // s3업로드 완료 확인시 로컬 저장소 파일 삭제
+            // 파일용량이 너무 작은 파일은 간헐적으로 0kb로 올라가는 경우가 있는데 이에대한 예외처리
+            for(int i=0; i<3; i++){
+                long filesize = amazonS3Client.getObjectMetadata(bucket, filename).getContentLength();
+                if(filesize <= 0){
+                    amazonS3Client.putObject(new PutObjectRequest(bucket, filename, files).withCannedAcl(CannedAccessControlList.PublicRead));
+                    Thread.sleep(300);
+                }else {
+                    log.info("s3 Upload done. fileSize : "+ filesize);
+                    break;
+                }
+            }
+            if (files.delete()) {
+                log.info("local file delete");
+            }
+        }else {
+            return false;
+        }
+        return true;
+    }
+
 
 }
